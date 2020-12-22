49a45a4762f96a59de2e08beabed9cd674762484
James Walker <walkah@walkah.net>
2020-12-21 19:37:54 -0500

    use fission-suite/kit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const kit = require('fission-kit')
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
-    extend: {},
+    colors: {
+      ...kit.dasherizeObjectKeys(kit.colors),
+    },
+    fontFamily: {
+      ...defaultTheme.fontFamily,
+
+      body: [kit.fonts.body, ...defaultTheme.fontFamily.sans],
+      display: [kit.fonts.display, ...defaultTheme.fontFamily.serif],
+      mono: [kit.fonts.mono, ...defaultTheme.fontFamily.mono],
+    },
   },
   variants: {
     opacity: ['group-hover'],
